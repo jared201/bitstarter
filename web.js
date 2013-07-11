b084131a54df7370e8120304ed984d81021a8590
Jared Odulio <jared.odulio@gmail.com>
2013-07-11 00:12:39 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var out = fs.readFileSync('index.html','utf-8');
+var buf = new Buffer(out);
+var str = buf.toString('utf-8',0,buf.lenght - 1);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
